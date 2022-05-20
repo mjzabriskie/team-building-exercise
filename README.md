38b0f0a192657b37b49c8097a8b89b74ff7bbaac
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-05-20 02:41:09 -0600

    Completed README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# team-building-exercise
+# Team-building Exercise
+
+## Description
+This is a command line application that allows a user to enter in a team roster and have it displayed nicely on a webpage. This project was created using TDD and OOP.
+
+## Installation
+* Clone repo to local directory
+* npm install
+
+## Testing
+Tests are included for each class. Jest was used to test and is listed as a devDependency.
+
+## Known Issues
+The max length add-on to inquirer seems to block the validation messages from appearing for more than a brief moment. More research is needed to find a solution to use both methods of validation.
+
+## Walkthrough
+Click [here](https://drive.google.com/file/d/1np_rz_EBAi40iYmU-FnlDObDyVsm71qx/view) to view a demonstration of the application.
--- src/page-template.js
+++ src/page-template.js
@@ -113,7 +113,7 @@ const generateHTML = (templateData) => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Team Generator</title>
+      <title>My Team</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet">
@@ -131,7 +131,7 @@ const generateHTML = (templateData) => {
         ${generateManager(templateData)}
         ${generateEmployees(templateData.employees)}
     </main>
-    
+
     <script src="https://kit.fontawesome.com/8286a8c819.js" crossorigin="anonymous"></script>
   </body>
   </html>
